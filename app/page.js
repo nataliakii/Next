e1f00c6b49c8fa4e3a5cf9c16349037acb7ec99e
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-01-02 18:40:22 +0200

    commit1

--- app/page.js
+++ app/page.js
@@ -64,19 +64,6 @@ export default function Home() {
             <p>Arazo Restaurant</p>
           </div>
         </Link>
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
